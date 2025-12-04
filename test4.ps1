16cda9a9a6d61a83113e4ea84fa293ec06973a34
Gabriel2025-source <GabrielApodaca@roundrocktexas.gov>
2025-12-04 13:24:04 -0600

    Create test4.ps1

---
+++ test4.ps1
@@ -0,0 +1,12 @@
+# Win11-Upgrade-Extracted.ps1
+# Run as Administrator
+
+$setupPath = "\\IT-GAPODACA\Win 11 Extracted iso\setup.exe"
+
+if (Test-Path $setupPath) {
+    Start-Process -FilePath $setupPath -ArgumentList "/auto upgrade /eula accept" -Wait
+    Write-Host "Windows 11 upgrade started silently from C:\Win11Upgrade."
+} else {
+    Write-Error "setup.exe not found in C:\Win11Upgrade"
+}
+
